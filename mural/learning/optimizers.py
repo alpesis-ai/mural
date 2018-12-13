5c60c7d16f1def46eb952411626630fceb98841b
K <kwailamchan@hotmail.com>
2018-12-13 17:04:02 +0800

    [master] setup learning rate in settings. 	modified:   mural/learning/optimizers.py 	modified:   mural/settings.py 	modified:   scripts/learning/mnist.sh

--- mural/learning/optimizers.py
+++ mural/learning/optimizers.py
@@ -1,21 +1,22 @@
+import settings
 from torch import optim
 
 
 def define_optimizer(name, model):
     if (name == "ADAM"):
-        optimizer = optim.Adam(model.parameters(), lr=0.003)
+        optimizer = optim.Adam(model.parameters(), lr=settings.LEARNING_RATE)
         return optimizer
 
     elif (name == "SGD"):
-        optimizer = optim.SGD(model.parameters(), lr=0.003)
+        optimizer = optim.SGD(model.parameters(), lr=settings.LEARNING_RATE)
         return optimizer
 
     elif (name == "ADAM_TRANS"):
-        optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
+        optimizer = optim.Adam(model.classifier.parameters(), lr=settings.LEARNING_RATE)
         return optimizer
 
     elif (name == "SGD_TRANS"):
-        optimizer = optim.SGD(model.classifier.parameters(), lr=0.003)
+        optimizer = optim.SGD(model.classifier.parameters(), lr=settings.LEARNING_RATE)
         return optimizer
 
     else:
--- scripts/learning/mnist.sh
+++ scripts/learning/mnist.sh
@@ -1,3 +1,3 @@
 python mural/main.py --dataset MNIST --model CLASSIFIER --loss NLL --optimizer ADAM --epochs 50 --learning VALID_STEPS
 
-python mural/main.py --dataset MNIST --model MLP --loss CROSSENTROPY --optimizer ADAM --epochs 2 --learning VALID_STEPS
+python mural/main.py --dataset MNIST --model MLP --loss CROSSENTROPY --optimizer SGD --epochs 2 --learning VALID_STEPS
